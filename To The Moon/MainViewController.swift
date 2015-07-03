513ec984feac03ae7ee2c80a60f775390d5f3fb1
Christoffer Buusmann <foffer@gmail.com>
2015-07-03 20:23:11 +0100

    Initial Commit

---
+++ To The Moon/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To The Moon
+//
+//  Created by Christoffer Buusmann on 03/07/2015.
+//  Copyright © 2015 Christoffer Buusmann. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
